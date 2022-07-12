a27fb87939d62c47b694562323c45f156e22f12c
scott <scott@justmachinery.net>
2022-07-12 01:04:36 -0700

    Updates and fixes for log importing

--- src/main/kotlin/net/justmachinery/CloudwatchLogsImporter.kt
+++ src/main/kotlin/net/justmachinery/CloudwatchLogsImporter.kt
@@ -15,7 +15,9 @@ import java.time.temporal.ChronoUnit
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.function.Consumer
+import kotlin.concurrent.thread
 import kotlin.math.max
+import kotlin.math.min
 
 @LogstashPlugin(name = "cloudwatch_logs_importer")
 class CloudwatchLogsImporter(
@@ -47,6 +49,14 @@ class CloudwatchLogsImporter(
             clientBuilder.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
         }
 
+        thread { //Periodically save plugin data, in case of a crash or something
+            while(stopped.count > 0){
+                if(!stopped.await(30, TimeUnit.MINUTES)){
+                    savePluginData()
+                }
+            }
+        }
+
 
         client = clientBuilder.build()
 
@@ -63,43 +73,62 @@ class CloudwatchLogsImporter(
         }
 
         var interval = 0L
-        var backwardsWindow = 60 * 60 * 1000L
-        while(stopped.count > 0){
-            try {
-                val seenForward = requestForwardLogs(processLog)
-                val seenBackward = requestBackwardLogs(processLog, backwardsWindow)
+        try {
+            while(stopped.count > 0){
+                try {
+                    synchronized(this){
+                        val seenForward = requestForwardLogs(processLog)
+                        val seenBackward = requestBackwardLogs(processLog)
 
-                backwardsWindow = if (seenBackward < targetEventsPerRequest) {
-                    (backwardsWindow * 1.2).toLong().coerceAtMost(365L * 24 * 60 * 60 * 1000)
-                } else {
-                    (backwardsWindow / 1.2).toLong().coerceAtLeast(1000)
+                        interval = if (seenForward + seenBackward < targetEventsPerRequest) {
+                            ((interval + 1000) * 1.2).toLong().coerceAtMost(maxPollingInterval)
+                        } else {
+                            0
+                        }
+                        log.info("Ingested $seenForward forward entries, $seenBackward backward entries. Sleeping for $interval ms")
+                    }
+                } catch(e : Exception){
+                    log.error("Could not fetch", e)
+                    interval = ((interval + 1000) * 1.2).toLong().coerceAtMost(maxPollingInterval)
                 }
-
-                interval = if (seenForward + seenBackward < targetEventsPerRequest) {
-                    ((interval + 1000) * 1.2).toLong().coerceAtMost(maxPollingInterval)
-                } else {
-                    0
-                }
-
-                log.debug("Ingested $seenForward forward entries, $seenBackward backward entries (new backwards window $backwardsWindow ms). Sleeping for $interval ms")
-            } catch(e : Exception){
-                log.error("Could not fetch", e)
-                interval = ((interval + 1000) * 1.2).toLong().coerceAtMost(maxPollingInterval)
+                stopped.await(interval, TimeUnit.MILLISECONDS)
             }
-            stopped.await(interval, TimeUnit.MILLISECONDS)
+        } finally {
+            stoppingDone.countDown()
+            log.info("Stopped ingesting")
         }
-        stoppingDone.countDown()
     }
 
     private fun requestForwardLogs(cb : (String, FilteredLogEvent)->Unit) : Int {
-        val request = FilterLogEventsRequest.builder()
-        request.startTime(data.latestTimestampSeen)
-        request.endTime(Instant.now().toEpochMilli())
         var seen = 0
         logGroups.forEach { logGroup ->
+            val request = FilterLogEventsRequest.builder()
             request.logGroupName(logGroup)
+
+            val now = Instant.now().toEpochMilli()
+            val start = synchronized(data){
+                val current = data.logGroupLastForward.getOrPut(logGroup){ 0L }
+                val fiveMinutesAgo = now - 300_000
+                //If we're more than five minutes behind, skip and create a gap
+                if(current < fiveMinutesAgo){
+                    logGroupTimestamps(logGroup).add(
+                        TimestampRange(
+                            lower = current,
+                            upper = fiveMinutesAgo
+                        )
+                    )
+                    data.logGroupLastForward[logGroup] = fiveMinutesAgo
+                    fiveMinutesAgo
+                } else {
+                    current
+                }
+            }
+
+            request.startTime(start)
+            request.endTime(now)
+
             var nextToken : String? = null
-            while(true) {
+            while(stopped.count > 0) {
                 if(nextToken != null){
                     request.nextToken(nextToken)
                 } else {
@@ -108,65 +137,113 @@ class CloudwatchLogsImporter(
                 val response = client.filterLogEvents(request.build())
                 response.events().forEach { event ->
                     cb(logGroup, event)
-                    data.latestTimestampSeen = max(event.timestamp(), data.latestTimestampSeen)
+                    data.logGroupLastForward[logGroup] = max(event.timestamp(), data.logGroupLastForward[logGroup] ?: 0L)
                     seen += 1
                 }
                 nextToken = response.nextToken()
                 if(nextToken == null) break
             }
+            if(stopped.count > 0){
+                data.logGroupLastForward[logGroup] = max(now, data.logGroupLastForward[logGroup] ?: 0L)
+            }
         }
         return seen
     }
 
-    private fun requestBackwardLogs(cb : (String, FilteredLogEvent)->Unit, backwardsWindow : Long) : Int {
-        if(data.earliestTimestampSeen == 0L){ return 0 }
-        if(backwardsLogFetchDays != null && data.earliestTimestampSeen < Instant.now().minus(backwardsLogFetchDays, ChronoUnit.DAYS).toEpochMilli()){
-            data.earliestTimestampSeen = 0
-            return 0
-        }
-
-        val earlier = max(data.earliestTimestampSeen - backwardsWindow, 0)
-
-        val request = FilterLogEventsRequest.builder()
-        request.startTime(earlier)
-        request.endTime(data.earliestTimestampSeen)
-
+    private val backwardsLogWindows = mutableMapOf<String, Long>()
+    private fun requestBackwardLogs(cb : (String, FilteredLogEvent)->Unit) : Int {
         var seen = 0
         logGroups.forEach { logGroup ->
-            request.logGroupName(logGroup)
-            var nextToken : String? = null
-            while(true) {
-                if(nextToken != null){
-                    request.nextToken(nextToken)
-                } else {
-                    request.nextToken(null)
+            fun processLogGroup() : Boolean {
+                val backwardsWindow = backwardsLogWindows.getOrPut(logGroup){ 60_000L}
+                val lastRange = synchronized(data){
+                    val ranges = logGroupTimestamps(logGroup)
+                    val current = ranges.lastOrNull() ?: return false
+                    current
                 }
-                val response = client.filterLogEvents(request.build())
-                response.events().forEach { event ->
-                    cb(logGroup, event)
-                    seen += 1
+                val earliestAllowed = backwardsLogFetchDays?.let { Instant.now().minus(it, ChronoUnit.DAYS).toEpochMilli() } ?: 0L
+                if(lastRange.upper < earliestAllowed){ return false }
+                val requestStart = (lastRange.upper - backwardsWindow).coerceAtLeast(lastRange.lower).coerceAtLeast(earliestAllowed)
+                val request = FilterLogEventsRequest.builder().apply {
+                    startTime(requestStart)
+                    endTime(lastRange.upper)
+                    logGroupName(logGroup)
                 }
-                nextToken = response.nextToken()
-                if(nextToken == null) break
-            }
-        }
 
-        data.earliestTimestampSeen = earlier
+                var logGroupSeen = 0
+                var nextToken : String? = null
+                while(stopped.count > 0) {
+                    if(nextToken != null){
+                        request.nextToken(nextToken)
+                    } else {
+                        request.nextToken(null)
+                    }
+                    val response = client.filterLogEvents(request.build())
+                    response.events().forEach { event ->
+                        cb(logGroup, event)
+                        logGroupSeen += 1
+                    }
+                    nextToken = response.nextToken()
+                    if(nextToken == null) break
+                }
+                if(stopped.count > 0){ //Only update if this range request finished
+                    lastRange.upper = requestStart
+                    //Merge overlapping ranges from back to front:
+                    synchronized(data){
+                        val ranges = logGroupTimestamps(logGroup)
+                        data.logGroupTimestamps[logGroup] = ranges.foldRight<TimestampRange, List<TimestampRange>>(listOf()){ earlier, acc ->
+                            if (earlier.upper <= earlier.lower) {
+                                acc
+                            } else if (acc.isEmpty()) {
+                                listOf(earlier)
+                            } else {
+                                val later = acc.last()
+                                if (later.lower <= earlier.upper) {
+                                    acc.dropLast(1) + TimestampRange(
+                                        lower = min(later.lower, earlier.lower),
+                                        upper = max(later.upper, earlier.upper)
+                                    )
+                                } else if (earlier.upper < earliestAllowed) {
+                                    acc
+                                } else {
+                                    acc + earlier
+                                }
+                            }
+                        }.asReversed().toMutableList()
+                    }
+
+                    backwardsLogWindows[logGroup] = if (logGroupSeen < targetEventsPerRequest) {
+                        (backwardsWindow * 1.2).toLong().coerceAtMost(365L * 24 * 60 * 60 * 1000)
+                    } else {
+                        (backwardsWindow / 1.2).toLong().coerceAtLeast(1000)
+                    }
+
+                }
+
+                seen += logGroupSeen
+
+                return true
+            }
+            @Suppress("ControlFlowWithEmptyBody")
+            while(stopped.count > 0 && seen < targetEventsPerRequest && processLogGroup()){}
+        }
 
         return seen
     }
+
+    private fun logGroupTimestamps(logGroup : String) = data.logGroupTimestamps.getOrPut(logGroup){ mutableListOf() }
 
     private val stopped = CountDownLatch(1)
     private val stoppingDone = CountDownLatch(1)
 
     override fun stop() {
         log.info("Stop requested")
-        savePluginData()
         stopped.countDown()
     }
 
     override fun awaitStop() {
         stoppingDone.await()
+        savePluginData()
         log.info("Stopped")
     }
 
@@ -177,19 +254,25 @@ class CloudwatchLogsImporter(
 
     private fun loadPluginData() : PluginData {
         return if(pluginDataPath.exists()){
-            Gson().fromJson(pluginDataPath.readText(), PluginData::class.java).also {
-                log.info("Loaded plugin data: $data")
+            try {
+                Gson().fromJson(pluginDataPath.readText(), PluginData::class.java).also {
+                    log.info("Loaded plugin data: $it")
+                }!!
+            } catch(t : Throwable){
+                log.error("Plugin data was corrupt or outdated, starting from scratch")
+                PluginData()
             }
         } else {
-            val now = Instant.now().toEpochMilli()
-            PluginData(now, now).also {
+            PluginData().also {
                 log.info("Created new plugin data")
             }
         }
     }
     private fun savePluginData() {
         try {
-            pluginDataPath.writeText(Gson().toJson(data))
+            synchronized(this){
+                pluginDataPath.writeText(Gson().toJson(data))
+            }
         } catch(e : Exception){
             log.error("Cannot save plugin data. Check that plugin_data_directory is writable and exists.", e)
         }
@@ -217,6 +300,11 @@ class CloudwatchLogsImporter(
 }
 
 private data class PluginData(
-    var latestTimestampSeen : Long,
-    var earliestTimestampSeen : Long
+    val logGroupTimestamps : MutableMap<String, MutableList<TimestampRange>> = mutableMapOf(),
+    val logGroupLastForward : MutableMap<String, Long> = mutableMapOf()
+)
+
+private data class TimestampRange(
+    var lower : Long,
+    var upper : Long
 )
